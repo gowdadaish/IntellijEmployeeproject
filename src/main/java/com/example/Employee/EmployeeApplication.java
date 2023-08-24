91f15293033d5d867b59049529b6f093be53b284
Aishwarya D <gowdaaish.d@gmail.com>
2023-08-24 12:16:29 -0400

    First version Employee

--- src/main/java/com/example/Employee/EmployeeApplication.java
+++ src/main/java/com/example/Employee/EmployeeApplication.java
@@ -3,9 +3,11 @@ package com.example.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan({"com.server", "com.server.config"})
 
 public class EmployeeApplication {
 
